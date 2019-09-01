daedefe4aa2fbb68bc33783e32518e38f44602b8
Luka Dimnik <luka.dimnik88@gmail.com>
2019-09-01 12:24:37 +0300

    minor changes

--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -3,18 +3,15 @@ package com.example.Bookstore.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BookController {
 
-	@GetMapping("/hello")
-	public String greeting(@RequestParam(value = "name") String name, @RequestParam(value = "age") String age,
-			Model model) {
-		model.addAttribute("name", name);
-		model.addAttribute("age", age);
+	@GetMapping("/index")
+	public String index() {
 		return "hello";
 
 	}
-
 }
